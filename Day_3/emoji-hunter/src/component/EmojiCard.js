a9f00398c876173150dc8945492606894544aa0f
praveen-me <praveenkrsaini0@gmail.com>
2018-10-22 16:14:58 +0530

    Add Copying Functionality

---
+++ Day_3/emoji-hunter/src/component/EmojiCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+class EmojiCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHoverId : 0
+    }
+  }
+
+  takeCopy = (e) => {
+    e.preventDefault();
+    const elm = e.target.firstChild;
+    navigator.clipboard.writeText(elm.innerHTML)
+  }
+
+  render() {
+    const {emoji} = this.props;
+    return (
+      <div className="EmojiCard" onClick={this.takeCopy}>
+        <div className="emoji-symbol inline">{emoji.symbol}</div>
+        <h2 className="emoji-title inline">{emoji.title}</h2>
+      </div>
+    );
+  }
+}
+
+export default EmojiCard;
