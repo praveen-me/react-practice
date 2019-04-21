8f7dd456b9a8444b70e579c9544db8e8c744373e
praveen-me <praveenkrsaini0@gmail.com>
2019-04-21 11:24:56 +0530

    Add uploadig file and style on on frontend

--- react-file-upload/client/src/App.js
+++ react-file-upload/client/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import FileUpload from './component/FileUpload';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container mt-4">
+        <h4 className="display-4 text-center mb-4">
+          <i className="fab fa-react"/> React File Upload
+        </h4>
+        <FileUpload />
       </div>
     );
   }
