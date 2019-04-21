b280c96443a3b7ff56538901a3edf0e2813ef9bf
praveen-me <praveenkrsaini0@gmail.com>
2019-04-21 12:53:55 +0530

    Add progress bar

---
+++ react-file-upload/client/src/component/Message.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Message = ({ msg }) => {
+  return (
+    <div className='alert alert-info alert-dismissible fade show' role='alert'>
+      { msg }
+    
+      <button
+      className="close"
+      data-dismiss="alert"
+      aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  )
+}
+
+Message.propTypes = {
+  msg: PropTypes.string.isRequired
+}
+
+export default Message
