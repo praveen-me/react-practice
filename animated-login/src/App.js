a5fbba654ca6c8758bf399a6638ee14bd9cfff3e
praveen-me <praveenkrsaini0@gmail.com>
2019-10-06 03:26:00 +0530

    Add animation to form

--- animated-login/src/App.js
+++ animated-login/src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [currentOpenSlide, setCurrentOpenSlide] = useState("signup");
+
+  const handleClick = slide => setCurrentOpenSlide(slide);
+
   return (
     <>
-      <div className="container" id="container">
+      <div
+        className={`container ${currentOpenSlide === "signin" &&
+          "right-panel-active"}`}
+        id="container">
         <div className="form-container sign-up-container">
           <form action="#">
             <h1>Create Account</h1>
@@ -22,7 +29,7 @@ function App() {
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <button onClick={() => handleClick("signup")}>Sign Up</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
@@ -43,7 +50,7 @@ function App() {
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
+            <button onClick={() => handleClick("signin")}>Sign In</button>
           </form>
         </div>
         <div className="overlay-container">
@@ -53,14 +60,20 @@ function App() {
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button className="ghost" id="signIn">
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleClick("signup")}>
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" id="signUp">
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={() => handleClick("signin")}>
                 Sign Up
               </button>
             </div>
