c978055f105cd02a92449777ee14d4c2a777807a
praveen-me <praveenkrsaini0@gmail.com>
2019-10-14 20:40:35 +0530

    Add full-stack graphql init files

--- kanban-board/src/normalized-state.js
+++ kanban-board/src/normalized-state.js
@@ -1,9 +1,11 @@
 import { schema, normalize } from 'normalizr';
-import defaultState from './default-state.json';
+import defaultState from './default-state';
 
 const user = new schema.Entity('users');
 const card = new schema.Entity('cards', { assignedTo: user });
-const list = new schema.Entity('lists', { cards: [card] });
+const list = new schema.Entity('lists', {
+  cards: [card],
+});
 
 const normalizedLists = normalize(defaultState.lists, [list]);
 const normalizedUsers = normalize(defaultState.users, [user]);
---
+++ kanban-board/src/reducers/list-reducer.js
@@ -0,0 +1,10 @@
+import { lists } from '../normalized-state';
+
+const listReducer = (state = lists, actions) => {
+  switch (actions.type) {
+    default:
+      return state;
+  }
+};
+
+export default listReducer;
