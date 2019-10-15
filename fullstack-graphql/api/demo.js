ee3b9123dad262124386901b1d2a25c60c87788e
praveen-me <praveenkrsaini0@gmail.com>
2019-10-16 00:41:27 +0530

    Add interface in graphql schema

--- fullstack-graphql/api/demo.js
+++ fullstack-graphql/api/demo.js
@@ -2,14 +2,38 @@ const gql = require("graphql-tag");
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = gql`
+  enum ShowType {
+    JORDAN
+    NIKE
+    ADIDDAS
+  }
+
   type User {
     email: String!
     password: String!
     friend: [User]
   }
 
+  interface Shoe {
+    brand: ShowType!
+    size: Int!
+  }
+
+  type Boot implements Shoe {
+    brand: ShowType!
+    size: Int!
+    hasGrip: Boolean!
+  }
+
+  type Sneakers implements Shoe {
+    brand: ShowType!
+    size: Int!
+    sport: String!
+  }
+
   type Query {
     me: User!
+    shoes: [Shoe]!
   }
 `;
 
@@ -21,10 +45,19 @@ const resolvers = {
         password: "Apple!",
         friend: []
       };
+    },
+    shoes() {
+      return [
+        { brand: "JORDAN", size: 14, hasGrip: true },
+        { brand: "NIKE", size: 12, sport: "Apple" }
+      ];
     }
   },
-  Mutation: {
-    addPet
+  Shoe: {
+    __resolveType(shoe) {
+      if (shoe.hasOwnProperty("hasGrip")) return "Boot";
+      return "Sneakers";
+    }
   }
 };
 
