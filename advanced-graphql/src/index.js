24ce01bc4cc9c52e983666146f728d3ca9de0729
praveen-me <praveenkrsaini0@gmail.com>
2019-10-19 18:44:15 +0530

    Add tests

--- advanced-graphql/src/index.js
+++ advanced-graphql/src/index.js
@@ -1,39 +1,44 @@
-const {ApolloServer} = require('apollo-server')
-const typeDefs = require('./typedefs')
-const resolvers = require('./resolvers')
-const {createToken, getUserFromToken} = require('./auth')
-const db = require('./db')
+const {
+  ApolloServer,
+  AuthenticationError,
+  UserInputError,
+  ApolloError
+} = require("apollo-server");
+const typeDefs = require("./typedefs");
+const resolvers = require("./resolvers");
+const { createToken, getUserFromToken } = require("./auth");
+const db = require("./db");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  async context({req, connection}) {        
+  async context({ req, connection }) {
     if (connection) {
-      return {...db, ...connection.context} 
+      return { ...db, ...connection.context };
     } else {
       const token = req.headers.authorization;
-      const user = getUserFromToken(token)
+      const user = getUserFromToken(token);
 
-      return {...db, user, createToken}
-    } 
-  }, 
+      return { ...db, user, createToken };
+    }
+  },
   subscriptions: {
     onConnect(connectionParams) {
       if (connectionParams.Authorization) {
-        const user = getUserFromToken(connectionParams.Authorization)
+        const user = getUserFromToken(connectionParams.Authorization);
 
         if (!user) {
-          throw new Error("No User Found")
+          throw new Error("No User Found");
         }
 
-        return { user }
+        return { user };
       }
 
-      throw new Error("not authenticated")
+      throw new Error("not authenticated");
     }
   }
-})
+});
 
-server.listen(4000).then(({url}) => {
-  console.log(`🚀 Server ready at ${url}`)
-})
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
--- advanced-graphql/src/resolvers.js
+++ advanced-graphql/src/resolvers.js
@@ -1,6 +1,6 @@
-const {PubSub} = require('apollo-server')
-const {authenticated, authorized} = require('./auth')
-const NEW_POST = 'NEW_POST'
+const { PubSub, AuthenticationError } = require("apollo-server");
+const { authenticated, authorized } = require("./auth");
+const NEW_POST = "NEW_POST";
 
 const pubSub = new PubSub();
 
@@ -11,90 +11,101 @@ const pubSub = new PubSub();
  */
 module.exports = {
   Query: {
-    me: authenticated((_, __, {user}) =>{
-      return user
+    me: authenticated((_, __, { user }) => {
+      return user;
     }),
-    posts(_, __, {user, models}) {
-      return models.Post.findMany({author: user.id})
+    posts(_, __, { user, models }) {
+      return models.Post.findMany({ author: user.id });
     },
 
-    post(_, {id}, {user, models}) {
-      return models.Post.findOne({id, author: user.id})
+    post(_, { id }, { user, models }) {
+      return models.Post.findOne({ id, author: user.id });
     },
 
-    userSettings(_, __, {user, models}) {
-      return models.Settings.findOne({user: user.id})
+    userSettings(_, __, { user, models }) {
+      return models.Settings.findOne({ user: user.id });
     },
     // public resolver
-    feed(_, __, {models}) {
-      return models.Post.findMany()
+    feed(_, __, { models }) {
+      return models.Post.findMany();
     }
   },
   Mutation: {
-    updateSettings(_, {input}, {user, models}) {
-      return models.Settings.updateOne({user: user.id}, input)
+    updateSettings(_, { input }, { user, models }) {
+      return models.Settings.updateOne({ user: user.id }, input);
     },
 
-    createPost(_, {input}, {user, models}) {
-      const post = models.Post.createOne({...input, author: user.id})
-      pubSub.publish(NEW_POST, { newPost: post })
-      return post
+    createPost(_, { input }, { user, models }) {
+      const post = models.Post.createOne({ ...input, author: user.id });
+      pubSub.publish(NEW_POST, { newPost: post });
+      return post;
     },
 
-    updateMe(_, {input}, {user, models}) {
-      return models.User.updateOne({id: user.id}, input)
+    updateMe(_, { input }, { user, models }) {
+      return models.User.updateOne({ id: user.id }, input);
     },
     // admin role
-    invite: authenticated(authorized("ADMIN", (_, {input}, {user}) => {
-      return {from: user, role: input.role, createdAt: Date.now(), email: input.email}
-    })),
+    invite: authenticated(
+      authorized("ADMIN", (_, { input }, { user }) => {
+        return {
+          from: user,
+          role: input.role,
+          createdAt: Date.now(),
+          email: input.email
+        };
+      })
+    ),
 
-    signup(_, {input}, {models, createToken}) {
-      const existing = models.User.findOne({email: input.email})
+    signup(_, { input }, { models, createToken }) {
+      const existing = models.User.findOne({ email: input.email });
 
       if (existing) {
-        throw new Error('nope')  
+        throw new AuthenticationError("User Already Found.");
       }
-      const user = models.User.createOne({...input, verified: false, avatar: 'http'})
-      const token = createToken(user)
-      return {token, user}
+      const user = models.User.createOne({
+        ...input,
+        verified: false,
+        avatar: "http"
+      });
+      const token = createToken(user);
+      return { token, user };
     },
-    signin(_, {input}, {models, createToken}) {
-      const user = models.User.findOne(input)
+    signin(_, { input }, { models, createToken }) {
+      const user = models.User.findOne(input);
 
       if (!user) {
-        throw new Error('nope')  
+        throw new AuthenticationError("User Not Found");
       }
 
-      const token = createToken(user)
-      return {token, user}
+      const token = createToken(user);
+      return { token, user };
     }
   },
   Subscription: {
     newPost: {
-      subscribe : () => pubSub.asyncIterator([NEW_POST])
+      subscribe: () => pubSub.asyncIterator([NEW_POST])
     }
   },
   User: {
-    posts(root, _, {user, models}) {
+    posts(root, _, { user, models }) {
       if (root.id !== user.id) {
-        throw new Error('nope')
+        throw new Error("nope");
       }
 
-      return models.Post.findMany({author: root.id})
+      return models.Post.findMany({ author: root.id });
     },
-    settings(root, __, {user, models}) {
-      return models.Settings.findOne({id: root.settings, user: user.id})
+    settings(root, __, { user, models }) {
+      return models.Settings.findOne({ id: root.settings, user: user.id });
     }
   },
   Settings: {
-    user(settings, _, {user, models}) {
-      return models.Settings.findOne({id: settings.id, user: user.id})
+    user(settings, _, { user, models }) {
+      return models.Settings.findOne({ id: settings.id, user: user.id });
     }
   },
   Post: {
-    author(post, _, {models}) {
-      return models.User.findOne({id: post.author})
+    author(post, _, { models }) {
+      return models.User.findOne({ id: post.author });
     }
   }
-}
+};
