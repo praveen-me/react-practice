b3af10c6683f61607ba97e8bebff9c1f988990a1
praveen-me <praveenkrsaini0@gmail.com>
2019-11-13 10:04:53 +0530

    Add authentication

--- amp-blog/graphql/config.js
+++ amp-blog/graphql/config.js
@@ -1,10 +1,30 @@
 import { withData } from 'next-apollo';
 import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+
+const authLink = setContext((_, { headers }) => {
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: `Basic cnRjYW1wOmdvb2R3b3Jr`
+		}
+	};
+});
 
 const config = {
-	link: new HttpLink({
-		uri: 'https://rtcamp-clone.dev5.rt.gw/graphql'
-	})
+	link: authLink.concat(
+		new HttpLink({
+			uri: 'https://rtcamp-clone.dev5.rt.gw/graphql'
+		})
+	),
+	request(operation) {
+		operation.setContext({
+			headers: {
+				authorization: `Basic cnRjYW1wOmdvb2R3b3Jr`
+			}
+		});
+	}
 };
 
 export default withData(config);
--- amp-blog/pages/index.js
+++ amp-blog/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAmp } from 'next/amp';
 import Head from 'next/head';
 import Nav from '../components/nav';
 import withData from './../graphql/config';
@@ -6,6 +7,10 @@ import './../scss/app.scss';
 import Postcard from '../components/PostCard';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+
+export const config = { amp: true };
+
+import { styleStr } from './../style';
 
 const query = gql`
 	query {
@@ -29,6 +34,7 @@ const query = gql`
 
 const Home = () => {
 	const { error, loading, data } = useQuery(query);
+	const isAmp = useAmp();
 
 	const posts = data?.posts?.nodes;
 
@@ -37,11 +43,13 @@ const Home = () => {
 			<Head>
 				<title>Home</title>
 				<link rel='icon' href='/favicon.ico' />
-				<link href='https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css' rel='stylesheet' />
+				<meta charSet='utf-8' />
+				{/* <amp stle */}
+				{isAmp && <style amp-custom={true}>{styleStr}</style>}
 			</Head>
 			<Nav />
 			<main className='main flex flex-wrap text-center my-8 mx-0'>
-				{posts && posts.map((post) => <Postcard {...post} key={post.id} />)}
+				{posts && posts.map((post) => <Postcard {...post} key={post.id} isAmp={isAmp} />)}
 			</main>
 		</div>
 	);
