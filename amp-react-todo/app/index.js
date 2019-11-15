d513226d9af5881ff354fcdfc0473e89f4848ce2
praveen-me <praveenkrsaini0@gmail.com>
2019-11-15 15:49:07 +0530

    Create delete functionality for todos

--- amp-react-todo/app/index.js
+++ amp-react-todo/app/index.js
@@ -1,6 +1,8 @@
 import { h, render } from 'preact';
 import App from './App';
 
+const btn = document.getElementById('btn');
+btn.style.display = 'none';
 const root = document.getElementById('root');
 
 render(<App />, root);
