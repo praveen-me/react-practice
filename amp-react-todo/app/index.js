a37049e14d98299cc8df70e0bdbe41dba188c38d
praveen-me <praveenkrsaini0@gmail.com>
2019-11-15 15:10:18 +0530

    Add prettier todo

--- amp-react-todo/app/index.js
+++ amp-react-todo/app/index.js
@@ -1,13 +1,8 @@
-import {h, render} from 'preact'
-import App from "./App";
+import { h, render } from 'preact';
+import App from './App';
 
-const btn = document.getElementById("btn");
-const root = document.getElementById("root");
+const btn = document.getElementById('btn');
+btn.style.display = 'none';
+const root = document.getElementById('root');
 
-btn.addEventListener("click", () => {
-  render(
-    <App />,
-    root
-  );
-  btn.style.display = "none";
-});
+render(<App />, root);
