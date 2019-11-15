2ef05de1f90749ca3df413965675e4f5920e32c7
praveen-me <praveenkrsaini0@gmail.com>
2019-11-15 15:20:59 +0530

    Remove unsed files

--- amp-react-todo/app/index.js
+++ amp-react-todo/app/index.js
@@ -1,8 +1,6 @@
 import { h, render } from 'preact';
 import App from './App';
 
-const btn = document.getElementById('btn');
-btn.style.display = 'none';
 const root = document.getElementById('root');
 
 render(<App />, root);
